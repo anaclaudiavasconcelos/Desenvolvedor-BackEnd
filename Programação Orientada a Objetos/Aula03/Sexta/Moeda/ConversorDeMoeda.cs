bcb2cf7244f0e040133a7d4ba4cdf27733dff61f
anaclaudiavasconcelos <anaclaudia32511@gmail.com>
2026-08-14 21:12:20 -0300

    Exercício Classe Static

---
+++ Programação Orientada a Objetos/Aula03/Sexta/Moeda/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Moeda
+{
+    internal static class ConversorDeMoeda
+    {
+        
+        public static double IOF = 0.06;
+
+
+
+        public static double Pagamento(double cotacao, double compra)
+        {
+            return cotacao * compra * (1 + IOF);
+        }
+    }
+
+
+}
---
+++ Programação Orientada a Objetos/Aula03/Sexta/Moeda/Program.cs
@@ -0,0 +1,15 @@
+﻿using Moeda;
+
+
+//Declaração de variáveis
+double cotacao, compra;
+
+//Entrada de dados
+Console.Write("Qual é a cotação do dolar? ");
+cotacao = double.Parse(Console.ReadLine());
+Console.Write("Quantos dólares você vai comprar? ");
+compra = double.Parse(Console.ReadLine());
+
+Console.Write($"Valor a ser pago em reias = {ConversorDeMoeda.Pagamento(cotacao, compra):f2}");
+
+Console.ReadKey();
