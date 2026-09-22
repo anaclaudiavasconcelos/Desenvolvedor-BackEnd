c74944a7cb6b7ff91b3d97d73be471ee1647f839
anaclaudiavasconcelos <anaclaudia32511@gmail.com>
2026-09-21 21:58:53 -0300

    Feito com banco de dados remoto.

---
+++ Banco de Dados/Aula07/ERPVendas/SistemaERP/Program.cs
@@ -0,0 +1,49 @@
+using SistemaERP.Classes.Contextos;
+
+namespace SistemaERP
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            
+            ContextoUsuario contexto = new ContextoUsuario();
+            contexto.Database.EnsureCreated();
+
+
+            if (TestarConexaoBanco())
+            {
+                MessageBox.Show("Conexão realizada com sucesso!");
+                Application.Run(new Login());
+                
+
+            }
+            else
+            {
+                MessageBox.Show("Falha ao conectar ao banco de dados");
+            }
+            
+        }
+
+        private static bool TestarConexaoBanco()
+        {
+            try
+            {
+                ContextoUsuario contexto = new ContextoUsuario();
+                return contexto.Database.CanConnect();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show($"Erro: {e.Message} ");
+                return false;
+            }
+        }
+    }
+}
